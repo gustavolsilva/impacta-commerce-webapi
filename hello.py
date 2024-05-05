8341a21f397a96782113b8162b5bf9e8e9800771
Gustavo <gustavolsilva@outlook.com>
2024-05-05 13:16:41 -0300

    update: criação da API que padroniza o Json dos Produtos

--- hello.py
+++ hello.py
@@ -1,7 +1,26 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/products")
+def products():
+    response = jsonify([
+        {
+            "title": "Caneca Personalizada de Porcelana Do Backend",
+            "amout": 123.45,
+            "installments": {"number": 3, "total": 41.15, "hasFee": True},
+        },
+        {
+            "title": "Caneca de Tulipa",
+            "amout": 123.45,
+            "installments": {"number": 3, "total": 41.15 },
+        }
+    ])
+
+    response.headers.add('Access-Control-Allow-Origin','*')
+
+    return response
